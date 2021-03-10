5d5bc51ce97d616aaff6fac25ae6b1fded5d2d96
lihonghui <honghui.li@shipparts.com>
2021-03-10 13:53:10 +0800

    158项目提交1

---
+++ src/main/java/com/git/bean/Student.java
@@ -0,0 +1,36 @@
+package com.git.bean;
+
+/**
+ * @author : lihonghui
+ * @date : 2021/3/10 13:51
+ * @Description :
+ **/
+public class Student {
+
+    private Integer id;
+    private String userName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
+}
